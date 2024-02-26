a7b77688593ea4bed725b9cb665ecf197f54c464
Matthew Schiller <mschille@nrao.edu>
2024-02-26 15:47:54 -0500

    Update Docker to automaticall pull the latest version of the underlying container.

--- fpga_vol/build_docker_2023.sh
+++ fpga_vol/build_docker_2023.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
-
+docker pull gcr.io/hdl-containers/sim/scipy:latest
 docker build -t fpga_vol:2023 -f Dockerfile_2023 .
